56ec31147fa5369f24672afb0c825cf6cb0e13f2
santi <santimezquita@hotmail.es>
2022-06-17 00:19:59 +0200

    fixing login

--- controllers/session/login.js
+++ controllers/session/login.js
@@ -2,18 +2,20 @@ const jwt = require("jsonwebtoken");
 const selectUser = require("../../connections/service/user/selectUser");
 
 async function checkLogin(req, res, next) {
+  console.log('en checklogin')
   try {
     console.log("Login controller");
     const credentials = req.body;
+    console.log(req.body)
     // Incorrect login___________________________________
     if (!credentials) {
       res.status(400).json({ error: "nice try" });
     }
 
     // Search user_______________________________________
-    const { id, email } = await selectUser(credentials);
+    const { id, email , username} = await selectUser(credentials);
 
-    if (!user) {
+    if (!id) {
       return res.status(400).json({ error: "credenciales incorrectas" });
     }
 
