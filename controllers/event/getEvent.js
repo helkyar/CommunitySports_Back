f8f00be72de68a19dcf80c1992f7a4fe2666e570
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-06-16 13:17:51 +0200

    Fix back break

--- controllers/event/getEvent.js
+++ controllers/event/getEvent.js
@@ -8,7 +8,7 @@ async function getEvent(req, res) {
 
   //(!) Validation
   const event = await selectEvent(data);
-  event.users = await selectEventUsers({ data });
+  if (event) event.users = await selectEventUsers({ data });
   //(!) Universal manager -> model response
   event !== null
     ? res.status(200).json({ event })
