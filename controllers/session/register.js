5de9877661cb1bd55b3a221093b18528c92fb858
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-17 06:22:19 +0200

    implemented token in register

--- controllers/session/register.js
+++ controllers/session/register.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const insertUser = require('../../connections/service/user/insertUser');
 const getAge = require("../../utility/getAge");
+const jwt = require("jsonwebtoken");
 const { v4 } = require('uuid')
 
 async function register(req, res, next) {
@@ -17,8 +18,9 @@ async function register(req, res, next) {
     const user = await insertUser(data);
     console.log(user);
     //(!) Universal manager -> model response
+    const token = jwt.sign({ id: data.id, username: data.username }, process.env.TOKEN_SECRET);
     user !== null
-      ? res.status(200).json(user)
+      ? res.status(200).json({ token: token, id: data.id, email: data.email })
       : res.status(400).json({ error: "Wrong format" });
   } catch (err) {
     next(err)
